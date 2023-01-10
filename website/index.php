aaacd231875f25950b442584c5b306999b0913cf
Uday <70462083+Uday599@users.noreply.github.com>
2023-01-10 21:15:03 +0530

    Update index.php

--- website/index.php
+++ website/index.php
@@ -1,8 +1,6 @@
-<html>
- <head>
-  <title>Index</title>
- </head>
- <body>
- <?php echo '<p>Hello world from PHP! Declarative pipeline!!</p>'; ?> 
- </body>
-</html>
+<?php
+
+require 'config.php';
+require 'functions.php';
+
+run();
